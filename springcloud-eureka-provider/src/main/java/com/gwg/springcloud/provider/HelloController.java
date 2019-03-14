eefdf6e6427b518b4c9402abf9fa058f8a67da0f
admin <weigang.gao>
2019-03-14 13:51:35 +0800

    完善

--- springcloud-eureka-provider/src/main/java/com/gwg/springcloud/provider/HelloController.java
+++ springcloud-eureka-provider/src/main/java/com/gwg/springcloud/provider/HelloController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Restful接口的第二种写法,在使用Eureka时候推荐，因为这样可以将服务提供方(IHelloRemote)单独抽象到一个模块中,服务提供方和使用方仅需要依赖
+ * Restful接口的第二种写法,在使用Eureka时候推荐，因为这样可以将Eureka服务提供方(IHelloRemote)单独抽象到一个模块中,
+ * 这样做，Eureka服务提供方方便实现并且可以提供resful服务和Eureka使用方仅需要依赖就可以使用了，当有新增服务的时候抽闲层仅需要改动一遍
  */
 @RestController
 public class HelloController implements IHelloRemote {
